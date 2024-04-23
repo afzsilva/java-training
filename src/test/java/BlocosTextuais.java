b2219942604e17b5b0e9e553b684bc482a7eb5dd
afzsilva <afzsilva@gmail.com>
2024-04-22 21:58:56 -0300

    Testes iniciais

---
+++ src/test/java/BlocosTextuais.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BlocosTextuais {
+
+    /*
+    Feature possivel apos java 15
+    * */
+    @Test
+    void deveMostrarEstrutuaBasicaHtmlSubstituindoParametros(){
+
+        String resultado = """
+                <html>
+                    <title>$titulo$</title>
+                    <body>
+                        <p>Seja bem vindo $usuario$</p>
+                    </body>
+                </html>
+                """.replace("$titulo$","Aprendendo Java").replace("$usuario$","Steve Jobs");
+
+
+
+        String esperado = """
+                <html>
+                    <title>Aprendendo Java</title>
+                    <body>
+                        <p>Seja bem vindo Steve Jobs</p>
+                    </body>
+                </html>
+                """;
+
+        Assertions.assertEquals(esperado, resultado);
+
+
+
+    }
+}
---
+++ src/test/java/ConcatenacaoTest.java
@@ -0,0 +1,68 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.text.MessageFormat;
+
+public class ConcatenacaoTest {
+
+    @Test
+    void deveConcatenarInvesMostrarTextoENumero(){
+
+        String str = "Entry#";
+        int index = 5;
+
+        /*leitura de esquerda para direita sem uso de parenteses
+        prevalece a concatenação*/
+
+        String result = str + index + 1;
+
+        Assertions.assertEquals("Entry#51", result);
+
+    }
+
+    @Test
+    void deveMostrarTextoENumero(){
+
+        String str = "Entry#";
+        int index = 5;
+
+        String result = str + (index + 1);//mostra string + resultado soma
+
+        //System.out.println(result);
+        Assertions.assertEquals("Entry#"+6, result);
+
+    }
+
+    @Test
+    void deveMostrarTextoENumeroUsandooStringFormat(){
+
+        String str = "Entry#";
+        int index = 5;
+
+        /*funciona porém pode degrada um pouco a performance usando String.format*/
+        String result = String.format("Entry#%d",index+1);//forma1
+        String result2 = MessageFormat.format("Entry#{0}", index+1);//forma2
+
+        //System.out.println(result);
+        Assertions.assertEquals("Entry#"+6, result);
+        Assertions.assertEquals("Entry#"+6, result2);
+
+
+    }
+
+    @Test
+    void deveMostrarTextoENumeroUsandooMessageFormat(){
+
+        String str = "Entry#";
+        int index = 5;
+
+        /*funciona porém pode degrada um pouco a performance usando MessageFormat*/
+        String result2 = MessageFormat.format("Entry#{0}", index+1);//forma2
+
+        //System.out.println(result);
+        Assertions.assertEquals("Entry#"+6, result2);
+
+
+    }
+
+}
